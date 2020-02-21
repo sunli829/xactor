1f97296040d56da6e1d700db97d52f42a776275e
sunli <scott_s829@163.com>
2020-02-21 10:21:15 +0800

    add LocalService

--- src/service.rs
+++ src/service.rs
@@ -3,13 +3,46 @@ use fnv::FnvHasher;
 use futures::lock::Mutex;
 use once_cell::sync::OnceCell;
 use std::any::{Any, TypeId};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::hash::BuildHasherDefault;
 
-/// Trait define a system service.
+/// Trait define a global service.
 ///
-/// The system service is a global actor.
+/// The service is a global actor.
 /// You can use `Actor::from_registry` to get the address `Addr<A>` of the service.
+///
+/// # Examples
+///
+/// ```rust
+/// use xactor::*;
+///
+/// #[message(result = "i32")]
+/// struct AddMsg(i32);
+///
+/// #[derive(Default)]
+/// struct MyService(i32);
+///
+/// impl Actor for MyService {}
+///
+/// impl Service for MyService {}
+///
+/// #[async_trait::async_trait]
+/// impl Handler<AddMsg> for MyService {
+///     async fn handle(&mut self, ctx: &Context<Self>, msg: AddMsg) -> i32 {
+///         self.0 += msg.0;
+///         self.0
+///     }
+/// }
+///
+/// #[async_std::main]
+/// async fn main() -> Result<()> {
+///     let mut addr = MyService::from_registry().await;
+///     assert_eq!(addr.call(AddMsg(1)).await?, 1);
+///     assert_eq!(addr.call(AddMsg(5)).await?, 6);
+///     Ok(())
+/// }
+/// ```
 #[async_trait::async_trait]
 pub trait Service: Actor + Default {
     async fn from_registry() -> Addr<Self> {
@@ -24,6 +57,38 @@ pub trait Service: Actor + Default {
             None => {
                 let addr = Self::default().start().await;
                 registry.insert(TypeId::of::<Self>(), Box::new(addr.clone()));
+                addr
+            }
+        }
+    }
+}
+
+thread_local! {
+    static LOCAL_REGISTRY: RefCell<HashMap<TypeId, Box<dyn Any + Send>, BuildHasherDefault<FnvHasher>>> = Default::default();
+}
+
+/// Trait define a local service.
+///
+/// The service is a thread local actor.
+/// You can use `Actor::from_registry` to get the address `Addr<A>` of the service.
+#[async_trait::async_trait]
+pub trait LocalService: Actor + Default {
+    async fn from_registry() -> Addr<Self> {
+        let res = LOCAL_REGISTRY.with(|registry| {
+            registry
+                .borrow_mut()
+                .get_mut(&TypeId::of::<Self>())
+                .map(|addr| addr.downcast_ref::<Addr<Self>>().unwrap().clone())
+        });
+        match res {
+            Some(addr) => addr,
+            None => {
+                let addr = Self::default().start().await;
+                LOCAL_REGISTRY.with(|registry| {
+                    registry
+                        .borrow_mut()
+                        .insert(TypeId::of::<Self>(), Box::new(addr.clone()));
+                });
                 addr
             }
         }
