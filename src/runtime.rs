72add955ff8e9fc5f7c8bb2dc38b38bebc7948c3
Alan Ramírez Herrera <alan5142@hotmail.com>
2020-12-07 18:35:20 -0600

    Tokio build fixed

--- src/runtime.rs
+++ src/runtime.rs
@@ -11,13 +11,13 @@ pub use async_std::{future::timeout, task::block_on, task::sleep, task::spawn};
 
 use futures::Future;
 #[cfg(feature = "runtime-tokio")]
-pub use tokio::{task::spawn, time::delay_for as sleep, time::timeout};
+pub use tokio::{task::spawn, time::sleep, time::timeout};
 
 #[cfg(feature = "runtime-tokio")]
 pub fn block_on<F, T>(future: F) -> T
 where
     F: Future<Output = T>,
 {
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(future)
 }
